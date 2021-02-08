57f5fea78789917d1bc2cef71ea439d7e8a53727
Jose Collas <joseherminiocollas@goatstone.com>
2021-02-08 14:14:01 -0800

    commit the build files

---
+++ www/precache-manifest.5925f1f213e0cdd5acce1ddc7ef8d7a3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5482ac4cc13eee4e7f37cd464f2a3f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13ea72828b7afd1b88e3",
+    "url": "/static/js/2.e318fbd3.chunk.js"
+  },
+  {
+    "revision": "0216cf33160466da7eb8",
+    "url": "/static/js/main.8c8c3f0d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
