9abaf485c9f57825ac2f712e3759873c8eed7adb
Jose Collas <joseherminiocollas@goatstone.com>
2021-02-14 18:55:41 -0800

    add build files

---
+++ www/precache-manifest.834fefdc81a4185a0754db9e82bcf417.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0643e0759de38a1215e173d20abb5f66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "980a494398ff7e32d318",
+    "url": "/static/css/main.ae6f52c9.chunk.css"
+  },
+  {
+    "revision": "4cf83dc25021dec4b473",
+    "url": "/static/js/2.553c5b40.chunk.js"
+  },
+  {
+    "revision": "980a494398ff7e32d318",
+    "url": "/static/js/main.b3ce0909.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
